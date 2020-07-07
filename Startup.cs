b370106b27232da5414a22034c3ea0a6d40f72ef
Odonno <bottiau.david@laposte.net>
2020-07-07 10:36:18 +0200

    feat: display swagger and redoc API documentation

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace DotnetCoreWebApiRedoc
 {
@@ -18,6 +22,22 @@ namespace DotnetCoreWebApiRedoc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "API",
+                        Version = "v1"
+                    }
+                );
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,6 +52,16 @@ namespace DotnetCoreWebApiRedoc
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
+            app.UseReDoc(c =>
+            {
+                c.SpecUrl = "/swagger/v1/swagger.json";
+            });
 
             app.UseEndpoints(endpoints =>
             {
